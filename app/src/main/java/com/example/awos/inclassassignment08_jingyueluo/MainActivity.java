0c4f9c772cb9d97d8c94de6b9b543c06861d32a6
Jingyue Luo <jl4964@tc.columbia.edugit config --global --listgit config --global user.name Jingyue Luo>
2018-04-17 21:43:53 -0400

    newly built

--- app/src/main/java/com/example/awos/inclassassignment08_jingyueluo/MainActivity.java
+++ app/src/main/java/com/example/awos/inclassassignment08_jingyueluo/MainActivity.java
@@ -2,38 +2,57 @@ package com.example.awos.inclassassignment08_jingyueluo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FirebaseAnalytics mFirebaseAnalytics;
-    // Obtain the FirebaseAnalytics instance.
-
-
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference databaseReference2 = FirebaseDatabase.getInstance().getReference("index2");
-
+    EditText keyArea;
+    EditText valueArea;
+    FirebaseDatabase database;
+    DatabaseReference myReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        keyArea = (EditText) findViewById(R.id.key_area);
+        valueArea = (EditText) findViewById(R.id.value_area);
+        database = FirebaseDatabase.getInstance();
+    }
 
-        databaseReference.setValue("lallaa");
-        databaseReference2.setValue("k");
+    public void writeToCloud(View view) {
+        myReference=database.getReference(keyArea.getText().toString());
+        myReference.setValue(valueArea.getText().toString());
+    }
 
+    public void readFromCloud(View view) {
+        myReference=database.getReference(keyArea.getText().toString());
+        myReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String loadedData = dataSnapshot.getValue(String.class);
+                    valueArea.setText(loadedData);
+                }else{
+                    valueArea.setText(null);
+                    Toast.makeText(MainActivity.this,"Cannot find the key",Toast.LENGTH_SHORT).show();
+                }
+            }
 
-        // Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-//
-//        myRef.setValue("Hello, World!");
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this,"Error loading",Toast.LENGTH_SHORT).show();
 
-
+            }
+        });
     }
 }
